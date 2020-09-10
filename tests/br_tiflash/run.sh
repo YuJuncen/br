4062d262fc43ab3abed919b430b5eac9583728d2
Hillium <maruruku@stu.csust.edu.cn>
2020-09-10 15:24:09 +0800

    tests: try to use tiup to bootstrap test cluster
    Signed-off-by: Hillium <maruruku@stu.csust.edu.cn>

--- tests/br_tiflash/run.sh
+++ tests/br_tiflash/run.sh
@@ -17,11 +17,23 @@ set -eu
 DB="${TEST_NAME}_DATABASE"
 RECORD_COUNT=1000
 
+tiflash_replica_ready() {
+   i=0
+   while ! grep "sum(AVAILABLE): 1" <(run_sql "select sum(AVAILABLE) from information_schema.tiflash_replica WHERE TABLE_NAME = $1"); do
+     i=$((i+1))
+     if [ "$i" -gt 10 ]; then
+         echo "wait tiflash replica ready timeout"
+         return 1
+     fi
+     sleep 30
+     run_sql "select sum(AVAILABLE) from information_schema.tiflash_replica WHERE TABLE_NAME = $1"
+   done
+ }
 
 run_sql "CREATE DATABASE $DB" 
 
-run_sql "CREATE TABLE $DB.kv(k varchar(256) primary key, v int)" 
-run_sql "ALTER TABLE $DB.kv SET TIFLASH REPLICA 1" 
+run_sql "CREATE TABLE $DB.kv(k varchar(256), v int primary key)"
+run_sql "ALTER TABLE $DB.kv SET TIFLASH REPLICA 1"
 
 stmt="INSERT INTO $DB.kv(k, v) VALUES ('1-record', 1)"
 for i in $(seq 2 $RECORD_COUNT); do
@@ -29,26 +41,7 @@ for i in $(seq 2 $RECORD_COUNT); do
 done
 run_sql "$stmt"
 
-i=0
-while ! [ $(run_sql "select * from information_schema.tiflash_replica" | grep "PROGRESS" | sed "s/[^0-9]//g") -eq 1 ]; do
-    i=$(( i + 1 ))
-    echo "Waiting for TiFlash synchronizing [$i]."
-    if [ $i -gt 20 ]; then
-        echo "Failed to sync data to tiflash."
-
-        # FIXME: current version of tiflash will fail on CI,
-        # that is, after TiFlash started, we cannot access :10080/tiflash/replicas
-        # our request will receive no response, hence TiFlash cannot work.
-        # We meet this problem after 2020/6/18, without modifing any test scripts.
-        # (see https://internal.pingcap.net/idc-jenkins/blue/organizations/jenkins/tidb_ghpr_integration_br_test/detail/tidb_ghpr_integration_br_test/1060/pipeline/106)
-        # This would probably be a bug of TiDB along with some mis-configurations.
-        # But today we cannot figure out what happened, and this would block many PRs, so we allow it pass for now.
-        # exit 1
-        echo "...but we must go on!"
-        break
-    fi
-    sleep 5
-done
+tiflash_replica_ready "'kv'"
 
 rm -rf "/${TEST_DIR}/$DB"
 run_br backup full -s "local://$TEST_DIR/$DB" --pd $PD_ADDR
@@ -56,20 +49,13 @@ run_br backup full -s "local://$TEST_DIR/$DB" --pd $PD_ADDR
 run_sql "DROP DATABASE $DB"
 run_br restore full -s "local://$TEST_DIR/$DB" --pd $PD_ADDR
 
-AFTER_BR_COUNT=`run_sql "SELECT count(*) FROM $DB.kv;" | sed -n "s/[^0-9]//g;/^[0-9]*$/p" | tail -n1`
-if [ $AFTER_BR_COUNT -ne $RECORD_COUNT ]; then
-    echo "failed to restore, before: $RECORD_COUNT; after: $AFTER_BR_COUNT"
-    exit 1
-fi
+tiflash_replica_ready "'kv'"
 
-# backup again, but don't remove tiflash replicas.
-run_br backup full -s "local://$TEST_DIR/$DB/with-tiflash" --pd $PD_ADDR --remove-tiflash=false
-run_sql "DROP DATABASE $DB"
-run_br restore full -s "local://$TEST_DIR/$DB/with-tiflash" --pd $PD_ADDR --remove-tiflash=false
-
-AFTER_BR_COUNT=`run_sql "SELECT count(*) FROM $DB.kv;" | sed -n "s/[^0-9]//g;/^[0-9]*$/p" | tail -n1`
-if [ $AFTER_BR_COUNT -ne $RECORD_COUNT ]; then
-    echo "failed to restore, before: $RECORD_COUNT; after: $AFTER_BR_COUNT"
+run_sql "SELECT /*+ read_from_storage(tiflash[$DB.kv]) */ count(*) FROM $DB.kv;"
+AFTER_BR_COUNT=`run_sql "SELECT /*+ read_from_storage(tiflash[kv]) */ count(*) FROM $DB.kv;" | sed -n "s/[^0-9]//g;/^[0-9]*$/p" | tail -n1`
+echo "before: $RECORD_COUNT; after: $AFTER_BR_COUNT"
+if [ $AFTER_BR_COUNT != $RECORD_COUNT ]; then
+    echo "...failed to restore!"
     exit 1
 fi
 
